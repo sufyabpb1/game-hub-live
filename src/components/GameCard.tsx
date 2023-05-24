0acad2b1b85122e20682f9d8510b670bd8ac9f1d
sufyan <sufyan.p@peerbits.com>
2023-05-24 14:10:05 +0530

    Displaying Platform Icons

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Game } from '../hooks/useGames'
-import { Card, CardBody, Heading, Image } from '@chakra-ui/react'
+import { Card, CardBody, Heading, Image, Text } from '@chakra-ui/react'
+import PlatformIconList from './PlatformIconList'
 
 interface Props {
     gamersss: Game
@@ -12,6 +13,7 @@ const GameCard = ({ gamersss }: Props) => {
             <Image src={gamersss.background_image}></Image>
             <CardBody>
                 <Heading fontSize="2xl">{gamersss.name}</Heading>
+                <PlatformIconList platforms={gamersss.parent_platforms.map(p => p.platform)}></PlatformIconList>
             </CardBody>
         </Card>
     )
---
+++ src/components/PlatformIconList.tsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import { Platform } from '../hooks/useGames'
+import { HStack, Icon, Text } from '@chakra-ui/react'
+import { FaWindows, FaPlaystation, FaXbox, FaApple, FaLinux, FaAndroid } from 'react-icons/fa'
+import { MdPhoneIphone } from 'react-icons/md'
+import { SiNintendo } from 'react-icons/si'
+import { BsGlobe } from 'react-icons/bs'
+import { IconType } from 'react-icons'
+interface Props {
+    platforms: Array<Platform>
+}
+const PlatformIconList = ({ platforms }: Props) => {
+    const iconMap: { [key: string]: IconType } = {
+        pc: FaWindows,
+        playstation: FaPlaystation,
+        xbox: FaXbox,
+        nintendo: SiNintendo,
+        mac: FaApple,
+        linux: FaLinux,
+        ios: MdPhoneIphone,
+        web: BsGlobe,
+        android: FaAndroid,
+    }
+    return (
+        <HStack marginY={1}>
+            {/* {platforms.map((platform) => <Text>{platform.name}</Text>)} */}
+            {platforms.map((platform) => <Icon as={iconMap[platform.slug]} color='gray.500'></Icon>)}
+        </HStack>
+    )
+}
+
+export default PlatformIconList
