9e5965c4de41ac914cf3b7eb704e6aaa8b2208a3
sufyan <sufyan.p@peerbits.com>
2023-05-25 19:13:18 +0530

    Show a spinner while fetching GenreList

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,10 +1,11 @@
-import { HStack, Image, List, ListItem, Text } from '@chakra-ui/react'
+import { HStack, Image, List, ListItem, Spinner, Text } from '@chakra-ui/react'
 import useGenres from '../hooks/useGenres'
 import getCroppedImageUrl from '../services/image-url'
 
 const GenreList = () => {
-    const { data } = useGenres()
+    const { data, isLoading } = useGenres()
     console.log("INSIDE THE GENRELIST  : : : ", data)
+    if (isLoading) return <Spinner />
     return (
         <List>
             {data.map((genre) =>
